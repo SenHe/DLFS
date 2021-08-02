9609c403d94e639177803aa54e52b5558c9fc71a
何森 <senhe752@gmail.com>
2021-08-03 00:32:10 +0100

    Update LATS_model.py

--- models_distan/LATS_model.py
+++ models_distan/LATS_model.py
@@ -197,30 +197,7 @@ class LATS(BaseModel): #Lifetime Age Transformation Synthesis
                 k2 = k
             params1[k].data.mul_(decay).add_(1 - decay, params2[k2].data)
 
-    def patchify_image(self, img, n_crop, min_size=1 / 8, max_size=1 / 4):
-        crop_size = torch.rand(n_crop) * (max_size - min_size) + min_size
-        batch, channel, height, width = img.shape
-        target_h = int(height * max_size)
-        target_w = int(width * max_size)
-        crop_h = (crop_size * height).type(torch.int64).tolist()
-        crop_w = (crop_size * width).type(torch.int64).tolist()
-
-        patches = []
-        for c_h, c_w in zip(crop_h, crop_w):
-            c_y = random.randrange(0, height - c_h)
-            c_x = random.randrange(0, width - c_w)
-
-            cropped = img[:, :, c_y : c_y + c_h, c_x : c_x + c_w]
-            cropped = F.interpolate(
-                cropped, size=(target_h, target_w), mode="bilinear", align_corners=False
-            )
-
-            patches.append(cropped)
-
-        patches = torch.stack(patches, 1).view(-1, channel, target_h, target_w)
-
-        return patches
-
+    
     def d_logistic_loss(self, real_pred, fake_pred):
         real_loss = F.softplus(-real_pred)
         fake_loss = F.softplus(fake_pred)
